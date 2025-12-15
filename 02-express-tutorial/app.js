8e98d467f5795adb3bbd1aa21a6064c9f1c64a7d
Elena <ek28150@gmail.com>
2025-12-14 18:35:22 -0800

    created a router and controller for people, modified app.js, installed morgan and used a logger

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const peopleRouter = require('./routes/people')
+
+// const morgan = require('morgan')
+
 const { products } = require('./data')
 
+const logger = (req,res,next) => {
+  const method = req.method;
+  const url =req.url;
+  const time = new Date().getFullYear()
+  console.log (method, url, time)
+  next()
+}
+
+// static resources
 app.use(express.static('./public'))
+// app.use(morgan('dev'))
+app.use(logger) 
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ message: "It worked!" });
@@ -12,8 +32,26 @@ app.get('/api/v1/products', (req, res) => {
   res.json(products)
 })
 
+// app.get('/api/v1/people', (req, res) => {
+//   res.json(people)
+// })
+
+// app.post('/api/v1/people', (req, res) => {
+//   if (!req.body.name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: "Please provide a name" })
+//   }
+//   else {
+//     people.push({ id: people.length + 1, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name });
+//   }
+// })
+
+app.use('/api/v1/people', peopleRouter)
+
+
 app.get('/api/v1/products/:productID',(req, res) => {
-  // res.json(req.params)
   const idToFind = parseInt(req.params.productID); 
   const product = products.find((p) => p.id === idToFind);
   if(!product){
@@ -21,8 +59,8 @@ app.get('/api/v1/products/:productID',(req, res) => {
   }
   return res.json(product)
 })
-app.get('/api/v1/query', (req, res)=>{
 
+app.get('/api/v1/query', (req, res)=>{
   const {search, limit, maxPrice} = req.query
   let sortedProducts = [...products]
 
@@ -34,7 +72,7 @@ app.get('/api/v1/query', (req, res)=>{
   if (limit) {
     sortedProducts = sortedProducts.slice(0,Number(limit))
   }
-  if(maxPrice){
+  if (maxPrice) {
     sortedProducts = sortedProducts.filter((product)=>{
       return product.price <= Number(maxPrice)
     })
