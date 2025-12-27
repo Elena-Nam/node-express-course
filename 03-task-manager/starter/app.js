8e8cabfd0d9ab35465b3fdd093007cd6422cc150
Elena <ek28150@gmail.com>
2025-12-26 20:57:02 -0800

    connected mongoDB, began task manager app

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,37 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res)=>{
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+/*
+app.get(`/api/v1/tasks`)
+app.post(`/api/v1/tasks`)
+app.get(`/api/v1/tasks/:id`)
+app.patch(`/api/v1/tasks/:id`)
+app.delete(`/api/v1/tasks/:id`)
+*/
+
+const port = 3000
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MongoDBUrl)
+        console.log("\n CONNECTED TO DB...")
+        app.listen(port, console.log(`Server is listening on ${port}...`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+start()
+// app.listen(port, console.log(`Server is listening on ${port}...`))
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,31 @@
+const Task = require("../models/tasks")
+
+const getAllTasks = (req, res) => {
+    res.send('all items from the file')
+}
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
+}
+
+const getTask = (req, res)=>{
+    res.json({id:req.params.id})
+}
+
+const updateTask = (req, res)=>{
+    res.send('update a task')
+}
+
+const deleteTask = (req, res)=>{
+    res.send('delete a task')
+}
+
+
+module.exports = {
+    getAllTasks, 
+    getTask, 
+    createTask, 
+    updateTask, 
+    deleteTask,
+}
