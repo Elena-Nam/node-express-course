5cb546478f24f2fa0f8f04feb211106e58949289
Elena <ek28150@gmail.com>
2026-01-19 17:45:38 -0800

    worked in starter/ 05-JWT-Basics folder, added authentication middleware, modified error classes

---
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -0,0 +1,22 @@
+const {UnAuthenticatedError} = require('../errors')
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = async(req, res, next) =>{
+  const authHeader = req.headers.authorization
+  if(!authHeader || !authHeader.startsWith('Bearer ')){
+    throw new UnAuthenticatedError ('No token provided')
+  }
+  
+  const token = authHeader.split(' ')[1]
+  
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const {id, username} = decoded
+    req.user = {id, username}  
+    next()
+  } catch(err) {
+    throw new UnAuthenticatedError ('Not authorized to access this route')
+  }
+}
+
+module.exports = authMiddleware
