b845f23442f29521932a93a9fd1e9e90675a6b29
Elena <ek28150@gmail.com>
2026-01-23 16:37:17 -0800

    added a 'return' in authMiddleware

--- 05-JWT-Basics/preferred/middleware/auth.js
+++ 05-JWT-Basics/preferred/middleware/auth.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken')
 const authMiddleware = async(req, res, next) =>{
   const authorization = req.headers.authorization
   if(!authorization || !authorization.startsWith('Bearer ')){
-    res.status(401).json({ message: 'Unauthorized'})
+    return res.status(401).json({ message: 'Unauthorized'}) 
+    // Needs to return after this otherwise it'll keep continuing to next lines of code which leads to a crash even though a response has been sent
+    // this middleware must stop execution if the auth header is bad.
   }
   
   const token = authorization.split(' ')[1]
